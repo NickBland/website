4a255dbd1c58071a46bd3fbfcd6cacfd746adad0
Nick Bland <nisky38@hotmail.com>
2022-04-12 00:10:06 +1000

    Add in new custom colour themes (same as splash colour scheme)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          100: '#6D327C',
+          200: '#4954AA',
+          300: '#0074CB',
+          400: '#0092D8',
+          500: '#00ABD0',
+          600: '#00C2B5',
+        },
+        light: {
+          100: '#F924F1',
+          200: '#FF11B1',
+          300: '#FF6079',
+          400: '#FF9D53',
+          500: '#FFCF4C',
+          600: '#F9F871',
+        },
+      }
+    },
   },
   plugins: [],
 }
