268a0ca69ec53c5aeee5f24b9e7acc27d62a49bf
Nick Bland <nick.bland@nickbland.dev>
2024-04-08 23:06:46 +1000

    Add more in to navbar
    + Add in some navigation tools
    + Add in responsive design

--- src/components/navbar/navbar.svelte
+++ src/components/navbar/navbar.svelte
@@ -2,9 +2,40 @@
   import ThemeController from "./themeController.svelte";
 </script>
 
-<div class="navbar bg-base-100">
-  <div class="flex-1 navbar-start">
-    <a class="btn btn-ghost text-xl" href="/">daisyUI</a>
+<div
+  class="navbar bg-base-300 dark:bg-neutral rounded-box mt-1 dark:text-neutral-content"
+>
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-5 h-5"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+      <ul
+        tabindex="-1"
+        class="tablist menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-300 dark:bg-neutral rounded-box w-52 dark:text-neutral-content"
+      >
+        <li><a href="/">Home</a></li>
+        <li><a href="/portfolio">Portfolio</a></li>
+      </ul>
+    </div>
+    <a class="btn btn-ghost text-xl" href="/">nickbland.dev</a>
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal px-1">
+      <li><a href="/">Home</a></li>
+      <li><a href="/portfolio">Portfolio</a></li>
+    </ul>
   </div>
   <div class="navbar-end">
     <ul class="menu menu-horizontal px-1">
