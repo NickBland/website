ef055880aac9c2be1f59745ee2ee7a9ffcda9591
Nick Bland <nick.bland@nickbland.dev>
2024-04-09 23:10:54 +1000

    Prep Utilised Pages
    Move navbar to shared layout.svelte so that it remains on all pages. Also keeps theme state.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	import '../app.css';
+  import "../app.css";
+  import Navbar from "../components/navbar/navbar.svelte";
 </script>
+
+<Navbar />
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,1 @@
-<script>
-  import Navbar from "../components/navbar/navbar.svelte";
-</script>
-
-<Navbar />
+<h1>Home</h1>
