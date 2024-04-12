15f17daada59873560cd995889ca41f35f3a7c7a
Nick Bland <nick.bland@nickbland.dev>
2024-04-12 17:43:16 +1000

    Quick Footer
    + Add generic footer

---
+++ src/components/footer.svelte
@@ -0,0 +1,11 @@
+<footer
+  class="footer items-center p-4 bg-base-300 dark:bg-neutral dark:text-neutral-content"
+>
+  <aside class="items-center grid-flow-col">
+    <a class="btn btn-ghost text-xl" href="/">nickbland.dev</a>
+    <p>Copyright © 2024 - All right reserved</p>
+  </aside>
+  <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+    <!-- Buttons Here -->
+  </nav>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import "../app.css";
   import Navbar from "../components/navbar/navbar.svelte";
+  import Footer from "../components/footer.svelte";
 </script>
 
 <Navbar />
 
 <slot />
+
+<Footer />
